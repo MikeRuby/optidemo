f8e25da09ce5f53815011e311d79189b9383f3be
Mike Ruby <emikebr@gmail.com>
2012-05-03 17:07:06 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Optidemo::Application.config.secret_token = 'f2bc33c6e8dc80897169ba86a5dd5949648324da7e87a058b595ba9769e6cc054ac5a5f035370fcb7ba74e11777433dcaf68d506b6f0f1492ff476a67fd9a696'
